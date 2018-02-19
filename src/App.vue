2765d3b0b15021298325dec5da8790586779444b
Roman Mazalov <dellight55@gmail.com>
2018-02-20 01:19:40 +0300

    Рефакторинг стилей для стартового меню.
    * Перенес стили из App в компонент стартового меню StartMenu.
    * Удалил стили в компоненте StartMenu, созданные по-умолчанию при создании проекта.

--- src/App.vue
+++ src/App.vue
@@ -26,38 +26,6 @@ body {
   padding: 0;
 }
 
-.menu {
-  flex: 1;
-  position: relative;
-}
-
-.menu h2 {
-  word-spacing: 8px;
-  color: #984231;
-  text-transform: uppercase;
-  font-size: 28px;
-}
-
-.menu h3 {
-  word-spacing: 8px;
-  color: #E5F499;
-  text-transform: uppercase;
-  font-size: 20px;
-}
-
-.menu p {
-  margin: 0;
-  word-spacing: 8px;
-  color: #E5F499;
-  text-transform: uppercase;
-  font-size: 20px;
-}
-
-.menuContent {
-  position: relative;
-  top: 35%;
-}
-
 #app {
   box-sizing: border-box;
   font-family: 'NintendoNES', Helvetica, Arial, sans-serif;
--- src/components/StartMenu.vue
+++ src/components/StartMenu.vue
@@ -36,15 +36,36 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.menu {
+  flex: 1;
+  position: relative;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.menu h2 {
+  word-spacing: 8px;
+  color: #984231;
+  text-transform: uppercase;
+  font-size: 28px;
 }
-a {
-  color: #42b983;
+
+.menu h3 {
+  word-spacing: 8px;
+  color: #E5F499;
+  text-transform: uppercase;
+  font-size: 20px;
+}
+
+.menu p {
+  margin: 0;
+  word-spacing: 8px;
+  color: #E5F499;
+  text-transform: uppercase;
+  font-size: 20px;
+}
+
+.menuContent {
+  position: relative;
+  top: 35%;
 }
 </style>
