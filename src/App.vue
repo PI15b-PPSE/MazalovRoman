ab94b6f8d4a9461e55179555d9ac0824479fd2b0
Roman Mazalov <dellight55@gmail.com>
2018-02-20 00:36:34 +0300

    Создал разметку стартового меню.
    * Добавлены стили для классов menu, menuContent, элемента body
    * Изменен стиль #app
    * Добавлен компонент StartMenu, создан шаблон для стартового меню.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div id="logo">
+      <img src="./assets/images/bdd_logo.gif">
+    </div>
     <router-view/>
   </div>
 </template>
@@ -12,12 +14,60 @@ export default {
 </script>
 
 <style>
+@@font-face {
+  font-family: "NintendoNES";
+  src: url('assets/fonts/nintendo_nes_font.ttf') format('ttf');
+}
+
+body {
+  background: #000;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
+.menu {
+  flex: 1;
+  position: relative;
+}
+
+.menu h2 {
+  word-spacing: 8px;
+  color: #984231;
+  text-transform: uppercase;
+  font-size: 28px;
+}
+
+.menu h3 {
+  word-spacing: 8px;
+  color: #E5F499;
+  text-transform: uppercase;
+  font-size: 20px;
+}
+
+.menu p {
+  margin: 0;
+  word-spacing: 8px;
+  color: #E5F499;
+  text-transform: uppercase;
+  font-size: 20px;
+}
+
+.menuContent {
+  position: relative;
+  top: 35%;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  box-sizing: border-box;
+  font-family: 'NintendoNES', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 40px 0;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 </style>
---
+++ src/components/StartMenu.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="menu">
+    <h2>{{ ultimateTeam }}</h2>
+    <div class="menuContent">
+    <h3>{{ copyright }}</h3>
+      <p v-for="item in menuContent">
+        {{ item.text }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StartMenu',
+  data () {
+    return {
+      ultimateTeam: '- the ultimate team -',
+      copyright: '\u00A92018 fuxy',
+      menuContent: [
+        { text: 'Licensed to tradewest by' },
+        { text: 'Rare coin-it, inc.' }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
