8d77c8fe0958e24aa00d6a09b5c43b297f5919cd
Roman Mazalov <dellight55@gmail.com>
2018-03-18 20:01:22 +0300

    Создал компонент для третьей страницы стартового меню.
    * Создал компонент StartMenuPage3 для третьей страницы стартового меню
    * Разметил каркас страницы в новом компоненте.

---
+++ src/components/StartMenuPage3.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrapper">
+      <div class="header-background">
+        <div class="header">
+
+        </div>
+        <div class="row">
+
+        </div>
+      </div>
+      <div class="row footer-background">
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StartMenuPage3'
+}
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-flow: column;
+  height: 100%;
+}
+
+.header {
+  flex: 0 1 auto;
+}
+
+.header-background {
+  display: contents;
+  background: url('@/assets/images/page3_tile_1.jpg') repeat;
+}
+
+.footer-background {
+  background: url('@/assets/images/page3_tile_2.jpg') repeat;
+}
+
+.row {
+  flex: 1 1 auto;
+}
+</style>
